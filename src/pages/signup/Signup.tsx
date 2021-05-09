2c08ec3aff19a458b1cadb6ca2f63157bdf83fd7
Guilherme Paes <guilhermep98@gmail.com>
2021-05-09 12:03:35 -0300

    WIP

--- src/pages/signup/Signup.tsx
+++ src/pages/signup/Signup.tsx
@@ -7,6 +7,9 @@ import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import Stepper from "@material-ui/core/Stepper";
 import Step from "@material-ui/core/Step";
 import StepLabel from "@material-ui/core/StepLabel";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import { TextField } from "@material-ui/core";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -15,10 +18,38 @@ const useStyles = makeStyles((theme: Theme) =>
       marginTop: "20px",
       overflow: "hidden",
       display: "flex",
-      height: "95vh",
     },
+   
   })
 );
+
+function getStepContent(stepIndex: number) {
+  switch (stepIndex) {
+    case 0:
+      return (
+        <Grid container spacing={2}>
+          <Grid item xs={6}>
+            <TextField label="Nome da propriedade" fullWidth/>
+          </Grid>
+          <Grid item xs={6}>
+          <TextField label="CNPJ" fullWidth/>
+          </Grid>
+          <Grid item xs={6}>
+            <TextField label="Endereço" fullWidth/>
+          </Grid>
+          <Grid item xs={6}>
+          <TextField label="Número de Quartos" fullWidth/>
+          </Grid>
+        </Grid>
+      ); 
+    case 1:
+      return "What is an ad group anyways?";
+    case 2:
+      return "This is the bit I really care about!";
+    default:
+      return "Unknown stepIndex";
+  }
+}
 
 const Signup = (): JSX.Element => {
   const classes = useStyles();
@@ -39,14 +70,40 @@ const Signup = (): JSX.Element => {
 
   return (
     <Paper className={classes.main}>
-      <Grid container spacing={4} direction="row" justify="center">
-        <Stepper activeStep={activeStep} alternativeLabel>
-          {steps.map((label) => (
-            <Step key={label}>
-              <StepLabel>{label}</StepLabel>
-            </Step>
-          ))}
-        </Stepper>
+      <Grid container spacing={2} direction="row" justify="center">
+        <Grid item xs={12}>
+          <Stepper activeStep={activeStep} alternativeLabel>
+            {steps.map((label) => (
+              <Step key={label}>
+                <StepLabel>{label}</StepLabel>
+              </Step>
+            ))}
+          </Stepper>
+        </Grid>
+        <Grid item xs={12}>
+          {activeStep === steps.length ? (
+            <div>
+              <Typography>All steps completed</Typography>
+              <Button onClick={handleReset}>Reset</Button>
+            </div>
+          ) : (
+            <div>
+              <Typography>{getStepContent(activeStep)}</Typography>
+              <div>
+                <Button disabled={activeStep === 0} onClick={handleBack}>
+                  Back
+                </Button>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={handleNext}
+                >
+                  {activeStep === steps.length - 1 ? "Finish" : "Next"}
+                </Button>
+              </div>
+            </div>
+          )}
+        </Grid>
       </Grid>
     </Paper>
   );
