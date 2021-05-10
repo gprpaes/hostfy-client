e7e6db2cff38929778f1eba884113f411c2da04d
Guilherme Paes <guilhermep98@gmail.com>
2021-05-10 02:57:36 -0300

    added admin route

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,16 @@ import Login from "./pages/login/components/Login";
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
 import Signup from "./pages/signup/Signup";
+import Admin from "./pages/admin/components/Admin";
 function App() {
   return (
     <div>
       <Switch>
         <Route path="/signup">
           <Signup />
+        </Route>
+        <Route path="/admin">
+          <Admin />
         </Route>
         <Route path="/">
           <Login />
