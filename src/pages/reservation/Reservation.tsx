846ca22d0b47850272b3f4e02bdb1e9acf4a3e82
Guilherme Paes <guilhermep98@gmail.com>
2021-05-10 04:13:11 -0300

    Added appa bar on bedrrom and reservation

--- src/pages/reservation/Reservation.tsx
+++ src/pages/reservation/Reservation.tsx
@@ -1,7 +1,64 @@
 import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import HotelIcon from "@material-ui/icons/Hotel";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import LaptopChromebookIcon from "@material-ui/icons/LaptopChromebook";
+import { useHistory } from "react-router-dom";
+import Grid from "@material-ui/core/Grid";
 
-const Reservation = (): JSX.Element => {
-  return <h1>reservas</h1>;
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+    gridmain: {
+      height: "93.2vh",
+    },
+
+    buttonmain: {
+      height: "500px",
+      width: "500px",
+      margin: "15px",
+    },
+  })
+);
+const Reservation
+ = (): JSX.Element => {
+  let history = useHistory();
+  const classes = useStyles();
+  return (
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <HotelIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Hostfy Administrador
+          </Typography>
+          <Button color="inherit" onClick={() => history.push("/")}>
+            Sair
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
 };
 
 export default Reservation
+;
